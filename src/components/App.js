bb5cd4a42340dc92b2bd0187d6257e08658a2b55
Nitin Tembhare <nitintembhare340@gmail.com>
2023-06-25 22:51:23 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React,{useState, useEffect} from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setdata] = useState([]);
+
+  function fetchData() {
+    axios.get("https://dummyjson.com/products")
+    .then((res)=>{setdata(res)})
+    .catch((err)=>{console.log(err); setdata(`An error occurred: ${err}`)})
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading product data...</p>;
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Data Fetched from API</h1>
+        <pre className="data">{JSON.stringify(data,null,2)}</pre>
     </div>
   )
 }
